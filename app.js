268e556f27ae4214b597a1884f24782f3ea22ef9
The Tuan <nttuan.20it6@vku.udn.vn>
2023-05-31 20:59:29 +0700

    add

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+require("dotenv").config()
+
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+console.log("Running");
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
