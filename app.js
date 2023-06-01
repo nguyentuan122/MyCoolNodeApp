6e95059c435dcbab2e63598c3aa7530554302b73
The Tuan <nttuan.20it6@vku.udn.vn>
2023-06-01 13:48:21 +0700

    hihi

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end('<h1>Hello World_NguyenTheTuan</h1>');
 });
 
 console.log("Running");
 
 server.listen(port,() => {
-  console.log(`Server running at port `+port);
+  console.log(`Server running at port http://localhost:`+port);
 });
